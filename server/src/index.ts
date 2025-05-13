d45048d00b5972861498727842a2011966a992fe
Vojtaner <vojtech.laurin@gmail.com>
2025-05-13 20:55:45 +0200

    try to mitigate database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,34 @@
 import { Request, Response } from "express";
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 const PORT = process.env.PORT || 8080;
-
+const todos = { text: "ahoj", id: 1 };
 app.use(cors());
 app.use(express.json());
 
 app.get("/todos", async (_req: Request, res: Response) => {
-  const todos = await prisma.todo.findMany();
+  // const todos = await prisma.todo.findMany();
   res.json(todos);
 });
 
 app.post("/todos", async (req: Request, res: Response): Promise<any> => {
-  const { text } = req.body;
-  if (!text) return res.status(400).json({ error: "Text is required" });
+  // const { text } = req.body;
+  // if (!text) return res.status(400).json({ error: "Text is required" });
 
-  const newTodo = await prisma.todo.create({
-    data: { text },
-  });
+  // const newTodo = await prisma.todo.create({
+  //   data: { text },
+  // });
 
-  res.status(201).json(newTodo);
+  res.status(201).json(todos);
+  // res.status(201).json(newTodo);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// "start": "npx tsc && npx prisma generate && node ./dist/index.js",
